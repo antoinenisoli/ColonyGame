b2074fd120762d07b6c9831d4e9b330d31e55527
Antoine Nisoli <nisoliantoine@outlook.com>
2022-12-15 11:32:27 +0100

    settler portraits

---
+++ Assets/Packages/Faces/SettlerPortrait.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+struct Replacable
+{
+	public string Name;
+	public Image image;
+	public Sprite[] sprites;
+}
+
+public class SettlerPortrait : MonoBehaviour 
+{
+	[SerializeField] Replacable[] portraitParts;
+
+	void Start() 
+	{
+		RandomizeCharacter();
+	}
+	
+	[ContextMenu(nameof(RandomizeCharacter))]
+	public void RandomizeCharacter()
+	{
+        foreach (var item in portraitParts)
+			item.image.sprite = item.sprites[Random.Range(0, item.sprites.Length)];
+	}
+
+}
